d3de9dceb0dfa349cd7dbc5a083f1c95ebfd6d17
Alexis King <lexi.lambda@gmail.com>
2017-05-15 20:11:01 -0700

    Add a simple hackett/function library with some combinators

---
+++ hackett-lib/hackett/function.rkt
@@ -0,0 +1,16 @@
+#lang hackett/private/kernel
+
+(provide id compose const flip)
+
+(def id : (∀ a (-> a a))
+  (λ [x] x))
+
+(def compose : (∀ a (∀ b (∀ c (-> (-> b c) (-> (-> a b) (-> a c))))))
+  (λ [f g x] (f (g x))))
+
+(def const : (∀ a (∀ b (-> a (-> b a))))
+  (λ [x y] x))
+
+(def flip : (∀ a (∀ b (∀ c (-> (-> a (-> b c))
+                               (-> b (-> a c))))))
+  (λ [f x y] (f y x)))
