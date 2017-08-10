c7539467e934297e0667c12704a7a811397eb789
Alexis King <lexi.lambda@gmail.com>
2017-08-10 15:11:39 -0700

    Render docs on Racket 6.8 to avoid racket/scribble#133

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev # exit with nonzero exit code if anything fails
 
-if [[ "$RACKET_VERSION" != 'HEAD' || "$TRAVIS_PULL_REQUEST" != 'false' || "$TRAVIS_BRANCH" != 'master' ]]; then
+if [[ "$RACKET_VERSION" != '6.8' || "$TRAVIS_PULL_REQUEST" != 'false' || "$TRAVIS_BRANCH" != 'master' ]]; then
   exit 0;
 fi
 
